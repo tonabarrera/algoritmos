e7bd487a91540ee9b8da471e5b4f52a5a74f182a
Tonatihu <carlostonatihu@gmail.com>
2017-11-15 19:51:53 -0600

    Esta vaina esta lista

--- 7/knapsack.cpp
+++ 7/knapsack.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <sstream>
 using namespace std;
 
 int objetos[2][2000];
@@ -10,25 +9,11 @@ int max_valor(int, int, int **);
 int mayor(int, int);
 
 int main(int argc, char const *argv[]) {
-    string str;
-    getline(cin, str);
-    stringstream is(str);
-    is >> capacidad;
+    cin >> capacidad >> cantidad;
     
-    is >> cantidad;
+    for (int i = 0; i < cantidad; i++)
+        cin >> objetos[0][i] >> objetos[1][i];
 
-    for (int i = 0; i < cantidad; i++) {
-        string str2;
-        getline(cin, str2);
-        stringstream is(str2);
-        int k;
-
-        is >> k;
-        objetos[0][i] = k;
-
-        is >> k;
-        objetos[1][i] = k;
-    }
     int **tabla = (int**) malloc((capacidad+1) * sizeof(int*));
     for (int i = 0; i < capacidad+1; i++){
         tabla[i] = (int*)malloc((cantidad+1) * sizeof(int));
--- 7/secuancia.cpp
+++ 7/secuancia.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int LCS(char *, char *);
-int max_numeros(int, int, int, int);
 
 int main(int argc, char const *argv[]){
+    // Captura de los datos
     char cadena1[1000];
     char cadena2[1000];
     cin >> cadena1;
@@ -44,29 +44,4 @@ int LCS(char *cadena1, char *cadena2) {
     }
 
     return tabla[anterior][longitud2];
-    //return max_numeros(tabla[actual][longitud2], tabla[actual][longitud2-1], tabla[anterior][longitud2], tabla[anterior][longitud2-1]);
-}
-
-int max_numeros(int a, int b, int c, int d) {
-    if (a > b)
-        if (a > c)
-            if (a > d)
-                return a;
-            else
-                return d;
-        else if (c > d)
-            return c;
-        else
-            return d;
-    else
-        if (b > c)
-            if (b > d)
-                return b;
-            else
-                return d;
-        else
-            if (c > d)
-                return c;
-            else
-                return d;
 }
