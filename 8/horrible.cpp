4db3ddea8f0979d3a947b37ad095f9570a40e2b0
Tonatihu <carlostonatihu@gmail.com>
2017-11-27 20:17:31 -0600

    Que chingados

--- 8/horrible.cpp
+++ 8/horrible.cpp
@@ -8,29 +8,30 @@ using namespace std;
 
 struct Nodo {
     unsigned long long valor;
-    unsigned long long indice;
+    int indice;
 };
 
-bool comparacion(struct Nodo a, struct Nodo b)
-{
+bool comparacion(struct Nodo a, struct Nodo b) {
     return (a.valor < b.valor);
 }
 
 int main(int argc, char const *argv[]) {
-    unsigned long long N, M, K;
+    int N, M, K;
     cin >> N >> M >> K;
     struct Nodo *entrada = (struct Nodo*)malloc(M * sizeof(struct Nodo));
     unsigned long long tipo, A, B;
-    for (unsigned long long i = 0; i < N; i++)
+
+    for (int i = 0; i < N; i++)
         cin >> tipo >> A >> B;
-    for (unsigned long long i=0; i < M; i++){
+    
+    for (int i = 0; i < M; i++){
         cin >> entrada[i].valor;
         entrada[i].indice = i+1;
     }
     if (K != 0){
         sort(entrada, entrada+M, comparacion);
-        for (unsigned long long i = M-K; i < M-1; i++)
-            cout << entrada[i].indice << " ";
+        for (int i = M-K; i < M-1; i++)
+            cout << entrada[i].indice << ", ";
         cout << entrada[M-1].indice; 
     }
     
